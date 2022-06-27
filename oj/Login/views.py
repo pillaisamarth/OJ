b97b34a937289fe45a8366be2128b30adb31c78f
Samarth <pillaisamarth@gmail.com>
2022-06-28 03:37:32 +0530

    all changes

--- oj/Login/views.py
+++ oj/Login/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
 
 def login(request):
-    return HttpResponse("HEllo there!")
-
-# Create your views here.
+    return HttpResponse("hps!")
