5a3651fde44fffb2af72957e4fb680dd9471eeb3
Samarth <pillaisamarth@gmail.com>
2022-06-28 03:36:09 +0530

    Added problem, solutions and testcase model

--- oj/Login/models.py
+++ oj/Login/models.py
@@ -1,10 +1,34 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    user_name = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+class Problem(models.Model):
+    title = models.CharField(max_length = 200)
+    statement = models.CharField(max_length=500)
+    difficulty = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
+
+class Solution(models.Model):
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    verdict = models.CharField(max_length=100)
+    submitted_at = models.DateTimeField(auto_now_add = True)
+    
+    def __str__(self) -> str:
+        return self.problem.title
+
+class TestCase(models.Model):
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    input = models.CharField(max_length=100)
+    output = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.problem.title
+
+
+
+    
 
 
